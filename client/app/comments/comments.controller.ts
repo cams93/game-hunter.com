590c68ee7b31b06f1e5bb30b0d0f2fba6d285dff
camendoz <cams.0093@gmail.com>
2016-05-02 22:20:25 -0500

    use game detail info from json

--- client/app/comments/comments.controller.ts
+++ client/app/comments/comments.controller.ts
@@ -14,31 +14,54 @@ angular.module('gameHunterApp')
     templateUrl: 'app/comments/comments.html',
     controller: CommentsComponent
   })
-  .controller('CommentsCtrl', function ($scope, $http) {
-        $scope.text = "";
-        $scope.comments = [];
+  .filter('split', function() {
+    return function(input, splitChar, splitIndex) {
+      if(input == null) {
+        return "TBA";
+      }
+      return input.split(splitChar)[splitIndex];
+    }
+  })
+  .controller('CommentsCtrl', function ($scope, $http, $giantbomb) {
+    $scope.text = "";
+    $scope.comments = [];
+    $scope.query = [];
+    $scope.description = "";
 
-        $scope.getComment = function(){
-          $http.get('/api/comments')
-            .success(function(data){
-              $scope.comments = data;
-            })
-            .error(function(){
-              console.log('Error!');
-            })
-        };
+    var callback = function(result){
+      $scope.query = result.results;
+      console.log(result.results);
+      $scope.description = $scope.query.description.replace(/<a href="[\/]/g,'<a href="http://www.giantbomb.com/');
+      return result;
+    };
 
-        $scope.postComment = function(){
-          $http.post('/api/comments', {commentData: $scope.text})
-           .success(function(){
+    $giantbomb.gameDetails("30615", callback);
 
-           })
-           .error(function(){
-             console.log('Error');
-           });
-           $scope.getComment();
-           $scope.text = '';
-        };
+    $scope.formatDescription = function(description){
+      return description;
+    };
 
-    });
+    $scope.getComment = function(){
+      $http.get('/api/comments')
+        .success(function(data){
+          $scope.comments = data;
+        })
+        .error(function(){
+          console.log('Error!');
+        })
+    };
+
+    $scope.postComment = function(){
+      $http.post('/api/comments', {commentData: $scope.text})
+       .success(function(){
+
+       })
+       .error(function(){
+         console.log('Error');
+       });
+       $scope.getComment();
+       $scope.text = '';
+    };
+
+  });
 })();
